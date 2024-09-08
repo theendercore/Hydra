9e194545a576caa0fc0929a9cb38aced4fb4bcd8
theendercore <theendercore@gmail.com>
2024-09-08 19:52:57 +0300

    - re-wrote command registry

--- src/main/kotlin/com/theendercore/hydra/commands/HydraCommand.kt
+++ src/main/kotlin/com/theendercore/hydra/commands/HydraCommand.kt
@@ -1,31 +1,56 @@
 package com.theendercore.hydra.commands
 
+import com.github.twitch4j.eventsub.events.ChannelFollowEvent
+import com.mojang.brigadier.CommandDispatcher
 import com.mojang.brigadier.context.CommandContext
+import com.theendercore.hydra.twitch.EventListeners
 import com.theendercore.hydra.twitch.TwitchBot
 import com.theendercore.hydra.util.Methods.addChatMsg
 import com.theendercore.hydra.util.Methods.playParticle
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource
 import net.minecraft.particle.ParticleTypes
 
 
 object HydraCommand {
+    fun register(dispatcher: CommandDispatcher<FabricClientCommandSource>) {
+        val hydraNode = literal("hydra").build()
+        dispatcher.root.addChild(hydraNode)
 
-    fun enable(): Int {
+        val enableNode = literal("enable")
+            .executes { enable() }
+            .build()
+        hydraNode.addChild(enableNode)
+
+        val disableNode = literal("disable")
+            .executes { disable() }
+            .build()
+        hydraNode.addChild(disableNode)
+
+        val testNode = literal("test")
+            .executes { test(it) }
+            .build()
+        hydraNode.addChild(testNode)
+    }
+
+    private fun enable(): Int {
         var x = 0
         Thread { x = TwitchBot.enable() }.start()
         return x
     }
 
-    fun disable(): Int {
+    private fun disable(): Int {
         return TwitchBot.disable()
     }
 
-    fun test(context: CommandContext<FabricClientCommandSource>): Int {
+    private fun test(context: CommandContext<FabricClientCommandSource>): Int {
         val source = context.source
         val player = source.player
 
         addChatMsg("Hello")
         playParticle(player, ParticleTypes.TOTEM_OF_UNDYING)
+
+        EventListeners.followingEventListener(ChannelFollowEvent())
 
         return 1
     }
--- src/main/kotlin/com/theendercore/hydra/init/CommandRegistry.kt
+++ src/main/kotlin/com/theendercore/hydra/init/CommandRegistry.kt
@@ -1,28 +1,12 @@
 package com.theendercore.hydra.init
 
 import com.theendercore.hydra.commands.HydraCommand
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback
 
 object CommandRegistry {
     fun init() {
         ClientCommandRegistrationCallback.EVENT.register(ClientCommandRegistrationCallback { dispatcher, _ ->
-            val hydraNode = ClientCommandManager.literal("hydra").build()
-            val enableNode = ClientCommandManager.literal("enable")
-                .executes { HydraCommand.enable() }
-                .build()
-            val disableNode = ClientCommandManager.literal("disable")
-                .executes { HydraCommand.disable() }
-                .build()
-            val testNode = ClientCommandManager.literal("test")
-                .executes { HydraCommand.test(it) }
-                .build()
-
-
-            dispatcher.root.addChild(hydraNode)
-            hydraNode.addChild(enableNode)
-            hydraNode.addChild(disableNode)
-            hydraNode.addChild(testNode)
+          HydraCommand.register(dispatcher)
         })
     }
 }
