fef98a4518faf359ef9e99fef3e2f2f7307ae57b
theendercore <theendercore@gmail.com>
2024-09-08 19:51:05 +0300

    - reformated AutoStart

--- src/main/kotlin/com/theendercore/hydra/util/AutoStart.kt
+++ src/main/kotlin/com/theendercore/hydra/util/AutoStart.kt
@@ -1,29 +1,22 @@
 package com.theendercore.hydra.util
 
-import com.theendercore.hydra.HydraMod.Companion.LOGGER
-import com.theendercore.hydra.HydraMod.Companion.MODID
-import com.theendercore.hydra.HydraMod.Companion.twitchClient
+import com.theendercore.hydra.HydraMod.LOGGER
+import com.theendercore.hydra.HydraMod.MODID
+import com.theendercore.hydra.HydraMod.twitchClient
 import com.theendercore.hydra.config.ModConfig
 import com.theendercore.hydra.twitch.TwitchBot
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents.ClientStopping
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents
-import net.minecraft.text.Text
-import net.minecraft.util.Formatting
 
 object AutoStart {
     fun init() {
-
-        ClientPlayConnectionEvents.JOIN.register(ClientPlayConnectionEvents.Join { _, _, _ ->
+        ClientPlayConnectionEvents.JOIN.register { _, _, _ ->
             if (ModConfig.config!!.autoStart && twitchClient == null) {
-                Methods.addChatMsg(
-                    Text.translatable("system.$MODID.auto_load").formatted(Formatting.DARK_GRAY)
-                )
-                Thread{
-                    TwitchBot.enable()
-                }.start()
+                Methods.addChatMsg(darkGrayText("system.$MODID.auto_load"))
+                Thread { TwitchBot.enable() }.start()
             }
-        })
+        }
 
         ClientLifecycleEvents.CLIENT_STOPPING.register(ClientStopping {
             TwitchBot.disable()
